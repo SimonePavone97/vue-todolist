6e2d4158591fd220abaf5c3ac754403b5aaa9773
SimonePavone <simonebest56@gmail.com>
2022-04-06 16:03:17 +0200

    Fine

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
-var app = new Vue (
-    {
-        el: '#root',
-        todo: {
-            'text' : '',
-            'done' : true
-        },
-        methods: {
+var app = new Vue({
+    el: "#root",
+    data: {
+        newTodo: '',
+        todos: [{
+                text: 'Ordinare pizza',
+                done: false
+            },
+            {
+                text: 'Fare spesa',
+                done: false
+            },
+            {
+                text: "Inviare mail al dott. Mimmo",
+                done: false
+            }
+        ],
+    },
+    methods: {
+        aggiungiTodo: function () {
+            const newItem = {
+                text: this.newTodo,
+                done: false
+            }
 
+            if (!this.todos.includes(newItem) && (newItem.text != '')) {
+                this.todos.push(newItem);
+            }
+
+            this.newTodo = '';
+        },
+        removeItem: function (index) {
+            this.todos.splice(index, 1);
+        },
+        todoDone: function (index) {
+            if (this.todos[index].done == false) {
+                this.todos[index].done = true;
+            } else {
+                this.todos[index].done = false;
+            }
         }
     }
-)
+})
